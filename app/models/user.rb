d7622c46bea93de50d79efe6bf295f07c71ebf6b
mitoo0403 <mitooo.19821113@gmail.com>
2020-12-09 17:11:17 +0900

    ユーザー管理機能についてのテストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
   Full_width_name = /\A[ぁ-んァ-ン一-龥]/
   Full_width_kana = /\A[ァ-ヶー－]+\z/
 
   validates :password, format: { with: VALID_PASSWORD_REGEX }
-  validates :nickname, presence: true, uniqueness: true
+  validates :nickname, presence: true
   validates :first_name, presence: true, format: { with: Full_width_name }
   validates :first_name_kana, presence: true, format: { with: Full_width_kana }
   validates :last_name, presence: true, format: { with: Full_width_name }
@@ -17,4 +17,3 @@ class User < ApplicationRecord
   validates :birthday, presence: true
          
 end
-
