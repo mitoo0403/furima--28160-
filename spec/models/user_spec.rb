f8e4ce3648dfe47002f0b4a4f99b0df9057cac82
mitoo0403 <mitooo.19821113@gmail.com>
2020-12-10 15:43:28 +0900

    テストの追加（passwordが英語のみ）

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -47,9 +47,15 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
       end
-      it "passwordが半角英数字混合でなければ登録できない" do
+      it "passwordが半角英数字混合でなければ登録できない（数字のみ）" do
         @user.password = "000000"
         @user.password_confirmation = "000000"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+      it "passwordが半角英数字混合でなければ登録できない(英語のみ)" do
+        @user.password = "aaaaaa"
+        @user.password_confirmation = "aaaaaa"
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is invalid")
       end
