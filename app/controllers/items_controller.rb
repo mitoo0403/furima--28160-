2a1867f7bed39a1e06205a59ca8c1dc227a10c1d
mitoo0403 <mitooo.19821113@gmail.com>
2021-01-07 11:29:01 +0900

    修正④

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, only: [:new, :create]
-
+  
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -16,6 +16,24 @@ class ItemsController < ApplicationController
       redirect_to root_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    if @item.user == current_user
+      render "edit"
+    else
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
     end
   end
 
